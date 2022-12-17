987a437580fb9b9043978a91d179d62f43fcccb9
aryanto16-rgb <113093156+aryanto16-rgb@users.noreply.github.com>
2022-12-17 17:28:34 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+		$("#satu").click(function(){
+			$(".container1").slideToggle();
+		});
+
+		$("#dua").click(function(){
+			$(".container2").slideToggle();
+		});
+
+		$("#tiga").click(function(){
+			$(".container3").slideToggle();
+		});
+
+		$("#empat").click(function(){
+			$(".container4").slideToggle();
+		});
+	});
